10125c04ee3fb7a1665937c94d18f86ca2d9ce20
Priscila Sartori <priscilasartori2@gmail.com>
2023-11-14 09:30:28 -0300

    feat: Criação da model

---
+++ Models/Extrato.cs
@@ -0,0 +1,11 @@
+namespace BankStatement {
+  public class Extrato {
+    public int Id {get; set;}
+    public string? Description {get; set;}
+    public string? Date {get; set;}
+    public int Value {get; set;}
+    public string? Loose {get; set;}
+    public string? Status {get; set;}
+
+  }
+}
